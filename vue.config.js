ce31e78f956126f11794cb760ee7854ab12167d5
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-21 21:01:35 +0900

    백 서버 연동을 위한 webpack개발서버 proxy지정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy: {
+      '/api' : {
+        target : 'http://localhost:8080', // 포워딩 할 서버의 주소
+        changeOrigin : true, // 오리진 변경 여부(포트 변경이므로 true)x
+      }
+    }
+  }
 })
