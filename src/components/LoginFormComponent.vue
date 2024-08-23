8a1429f0dd457aa42445641ddc9f538743f8bfac
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-23 19:03:31 +0900

    Axios 라이브러리 추가 및 로그인 서버통신 로직 추가

--- src/components/LoginFormComponent.vue
+++ src/components/LoginFormComponent.vue
@@ -2,12 +2,13 @@
   <div class="loginForm">
     <input v-model="userId" placeholder="아이디 입력" type="text" />
     <input v-model="password" placeholder="비밀번호 입력" type="password">
-    <button>로그인</button>
+    <button v-on:click="getLogin">로그인</button>
   </div>
 </template>
 
 <script>
 import {ref} from "vue";
+import axios from "axios";
 
 export default {
   name: 'LoginFormComponent', // 필수 X
@@ -18,8 +19,22 @@ export default {
     let userId = ref('');
     let password = ref('');
 
+    // 템플릿에서 사용할 함수 표현식 정의
+    const getLogin = () => {
+      // 서버로의 통신
+      axios.get("http://localhost:8080/api/login", {
+        params:{
+          userId : userId.value,
+          password : password.value
+        }
+      }).then((response)=>{
 
-    return {userId, 'password':password}; //두 속성은 동일
+        console.log(response)
+      })
+    }
+
+
+    return {userId, 'password':password, getLogin}; //두 속성은 동일
   }
 }
 </script>
@@ -29,10 +44,17 @@ export default {
 .loginForm{
   /*img파일과 거리두기*/
   margin: 100px;
+
+
 }
 button{
   /* 버튼 Block속성 지정*/
   display: block;
+  /* margine-top 0 , center*/
+  margin: 0 auto;
+  /* buuton border 두께, 스타일, 컬러*/
+  border: 1px solid #ddd;
+  background-color: #fff;
 }
 
 input{
