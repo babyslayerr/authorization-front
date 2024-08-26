4c5af2272e6e21c5b698366b26169927ea8087e9
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-08-27 03:01:28 +0900

    vue router 추가 및 설정, 간단한 화면 routing 테스트 화면 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import MainPage from "@/pages/MainPage.vue"; // routing 을 위한 vue 라우터 추가
 
-createApp(App).mount('#app')
+// vue createApp 함수에 사용할 router 상수 추가
+let router = createRouter({
+    // Vue 3는 mode 속성 대신 history 속성 사용
+    history: createWebHistory(), // createMemoryHistory()은 메모리 history 로 SSR 에서 사용 됨
+    routes: [ // 컴포넌트와 URL 매핑 설정
+        {path:'/home', component : MainPage}
+    ]
+})
+
+
+createApp(App)
+    .use(router) // vue 앱 생성시 router 추가
+    .mount('#app')
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>이 화면은 메인페이지 입니다.</div>
+</template>
+
+<style scoped>
+
+</style>
