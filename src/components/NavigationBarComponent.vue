715e4a556e020816015568e566e205b38a3b2292
JongHwanJeong <96283391+babyslayerr@users.noreply.github.com>
2024-09-03 16:44:51 +0900

    add annotation

--- src/components/NavigationBarComponent.vue
+++ src/components/NavigationBarComponent.vue
@@ -6,8 +6,8 @@
 <template>
   <div>
     <router-link to="/">to Home</router-link> <!-- 페이지 이동 링크, a태그로 변형되서 나옴 -->
-    <router-link to="/login">to Login</router-link>
-    <router-link to="/signUp">to signUp</router-link> <!-- 휘원가입 페이지 링크-->
+    <router-link to="/login">to Login</router-link><!--로그인 페이지 링크 -->
+    <router-link to="/signUp">to signUp</router-link> <!-- 회원가입 페이지 링크-->
   </div>
 </template>
 
